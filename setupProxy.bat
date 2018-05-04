5ace9c72bd5c4218d18ffa6f9bf2925000b008f0
unknown <pedro.ze.lagarto@gmail.com>
2018-05-04 18:51:35 +0100

    no path for you

--- setupProxy.bat
+++
@@ -1,6 +0,0 @@
-
-set PATH=%PATH%;C:\Users\i052055\Desktop\ESCOLA_SIGOM\2017_2018\LatestNode
-
-npm config set proxy "http://proxy.totta.gs.corp:8080"
-
-npm config set https-proxy "http://proxy.totta.gs.corp:8080"
